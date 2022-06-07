b941684839a665c1d1b3df038d4b0e57f2823102
Kshitij Aranke <kshitij@aranke.org>
2022-06-06 18:03:35 -0700

    code cleanup

--- dbt_yaml_check/main.py
+++ dbt_yaml_check/main.py
@@ -7,11 +7,6 @@ from tabulate import tabulate
 
 app = typer.Typer(add_completion=False)
 
-TABLE_OPTIONS = {
-    "headers": ["model", "column"],
-    "tablefmt": "pretty",
-}
-
 
 @app.callback(invoke_without_command=True)
 def callback(
@@ -27,35 +22,31 @@ def callback(
 
     error_on_exit = False
 
-    extra_columns = []
+    table_columns = []
 
     if manifest_file.exists() and catalog_file.exists():
         manifest = json.load(open(manifest_file))
         catalog = json.load(open(catalog_file))
 
         manifest_nodes = {
-            k.lower(): v
+            k.lower(): [c.lower() for c in v["columns"].keys()]
             for k, v in manifest["nodes"].items()
             if v["resource_type"] != "test"
         }
 
-        catalog_nodes = {k.lower(): v for k, v in catalog["nodes"].items()}
+        catalog_nodes = {
+            k.lower(): [c.lower() for c in v["columns"].keys()]
+            for k, v in catalog["nodes"].items()
+        }
 
         for node in manifest_nodes:
             if node not in catalog_nodes:
-                extra_columns.append((node.split(".")[-1], ""))
+                table_columns.append((node.split(".")[-1], ""))
                 error_on_exit = True
             else:
-                manifest_columns = [
-                    c.lower() for c in manifest_nodes[node]["columns"].keys()
-                ]
-                catalog_columns = [
-                    c.lower() for c in catalog_nodes[node]["columns"].keys()
-                ]
-
-                for column in manifest_columns:
-                    if column not in catalog_columns:
-                        extra_columns.append((node.split(".")[-1], column))
+                for column in manifest_nodes[node]:
+                    if column not in catalog_nodes[node]:
+                        table_columns.append((node.split(".")[-1], column))
                         error_on_exit = True
     else:
         typer.echo(
@@ -63,10 +54,10 @@ def callback(
         )
         error_on_exit = True
 
-    if extra_columns:
+    if table_columns:
         typer.secho(
             tabulate(
-                sorted(extra_columns, key=itemgetter(1, 0)),
+                sorted(table_columns, key=itemgetter(1, 0)),
                 headers=["Missing SQL Node", "Missing SQL Column (If Applicable)"],
                 tablefmt="pretty",
             ),
